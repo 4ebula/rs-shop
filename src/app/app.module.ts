13adf2882e35cec293d2c9ce1dbaa13981139989
4ebula <4ebula@gmail.com>
2021-09-10 18:25:15 +0300

    feat: add core module and default styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from '@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { InlineSVGModule } from 'ng-inline-svg';
+import { CategoryNavigationComponent } from './components/category-navigation/category-navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, CategoryNavigationComponent],
+  imports: [CommonModule, InlineSVGModule.forRoot()],
+  exports: [HeaderComponent],
+})
+export class CoreModule {}
