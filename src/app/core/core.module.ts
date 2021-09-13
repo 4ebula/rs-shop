9f93fbd79829a2d94fb1f008716d0d84b2b66b71
4ebula <4ebula@gmail.com>
2021-09-13 23:38:34 +0300

    feat: add home redirection on logo

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { InlineSVGModule } from 'ng-inline-svg';
 import { CategoryNavigationComponent } from './components/category-navigation/category-navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HeaderInfoComponent } from './components/header-info/header-info.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CoreRouterModule } from './core-routing.module';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, CategoryNavigationComponent, HeaderInfoComponent],
-  imports: [CommonModule, InlineSVGModule.forRoot()],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    CategoryNavigationComponent,
+    HeaderInfoComponent,
+  ],
+  imports: [CommonModule, CoreRouterModule, InlineSVGModule.forRoot()],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
