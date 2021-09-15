186f1f5b2dfb62a5d506bd11ec7836d72405449b
4ebula <4ebula@gmail.com>
2021-09-15 05:10:24 +0300

    feat: change markup in category navigation to service data

--- src/app/core/components/category-navigation/category-navigation.component.ts
+++ src/app/core/components/category-navigation/category-navigation.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ICategoryResponse } from '@core/models/category-response.model';
+import { CategoryService } from '@core/services/category/category.service';
 
 @Component({
   selector: 'app-category-navigation',
   templateUrl: './category-navigation.component.html',
-  styleUrls: ['./category-navigation.component.scss']
+  styleUrls: ['./category-navigation.component.scss'],
 })
 export class CategoryNavigationComponent implements OnInit {
+  categories!: ICategoryResponse[];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.categories = this.categoryService.getCategories();
   }
-
 }
