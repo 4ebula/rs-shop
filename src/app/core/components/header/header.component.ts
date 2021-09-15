883412bb5d404af88e75b6e0715e869ec8be0cd0
4ebula <4ebula@gmail.com>
2021-09-15 17:06:33 +0300

    feat: add closing popup on navigation

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   isCategoriesOpened: boolean = false;
 
+  sub!: Subscription;
+
+  constructor(private router: Router) {}
+
   showCategories(): void {
-    this.isCategoriesOpened = true;
+    this.isCategoriesOpened = !this.isCategoriesOpened;
   }
 
   closePopup(): void {
     this.isCategoriesOpened = false;
+  }
+
+  ngOnInit(): void {
+    this.sub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.closePopup();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
