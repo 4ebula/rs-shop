f07f1a987d62c55c88679189655998626e73d0f8
4ebula <4ebula@gmail.com>
2021-09-17 19:56:46 +0300

    feat: add error handling to geo service

--- src/app/core/services/geo/geo.service.ts
+++ src/app/core/services/geo/geo.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
+const DEFAULT_LOCATION = 'Минск';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -10,27 +12,19 @@ export class GeoService {
 
   public geo$ = this.geo.asObservable();
 
-  geo1!: string;
-
   constructor(private http: HttpClient) {
-    this.getGeo1().subscribe((res) => {
-      console.log(res.city.name_ru);
-      this.geo1 = res.city.name_ru;
-      this.geo.next(res.city.name_ru);
-    });
+    this.getGeo().subscribe(
+      (response) => {
+        this.geo.next(response.city.name_ru);
+      },
+      (error) => {
+        this.geo.next(DEFAULT_LOCATION);
+        throw error;
+      },
+    );
   }
 
-  async getGeo(): Promise<void> {
-    const num1 = this.http.get('https://api.sypexgeo.net');
-    console.log(num1);
-    const response = await fetch('https://api.sypexgeo.net');
-    const data = await response.json();
-    const city = data.city.name_ru;
-    this.geo.next(city);
-    return city;
-  }
-
-  getGeo1(): Observable<any> {
+  getGeo(): Observable<any> {
     return this.http.get('https://api.sypexgeo.net');
   }
 }
