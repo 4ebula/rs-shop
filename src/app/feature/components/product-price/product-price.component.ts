48035a5433ef77b7409d1d0e81ab0530d8f34a2e
4ebula <4ebula@gmail.com>
2021-09-19 03:27:18 +0300

    feat: add markup to price component

--- src/app/feature/components/product-price/product-price.component.ts
+++ src/app/feature/components/product-price/product-price.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-price',
   templateUrl: './product-price.component.html',
   styleUrls: ['./product-price.component.scss'],
 })
-export class ProductPriceComponent {
+export class ProductPriceComponent implements OnInit {
   @Input() price!: number;
 
   @Input() rating!: number;
 
-  @Input() isInCategory!: boolean;
+  @Input() isInProduct!: boolean;
+
+  constructor(private elemRef: ElementRef) {}
+
+  ngOnInit(): void {
+    if (this.isInProduct) this.elemRef.nativeElement.classList.add('product');
+  }
 
   createArr = (length: number): Array<any> => {
     return Array.from({ length });
