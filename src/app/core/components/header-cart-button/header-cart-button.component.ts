cf2120b8fecd2594ab3fecac0ef3ac6c7fa1ff08
4ebula <4ebula@gmail.com>
2021-09-20 13:16:57 +0300

    feat: add add-to-cart animation

--- src/app/core/components/header-cart-button/header-cart-button.component.ts
+++ src/app/core/components/header-cart-button/header-cart-button.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/feature/services/cart/cart.service';
 
 @Component({
   selector: 'app-header-cart-button',
   templateUrl: './header-cart-button.component.html',
   styleUrls: ['./header-cart-button.component.scss'],
 })
-export class HeaderCartButtonComponent {}
+export class HeaderCartButtonComponent implements OnInit {
+  amountInCart: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    // eslint-disable-next-line no-plusplus
+    this.cartService.cartContent$.subscribe(() => this.amountInCart++);
+  }
+
+  checkIfCartHasProducts(): boolean {
+    return this.amountInCart > 0;
+  }
+}
