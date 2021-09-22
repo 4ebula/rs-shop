27cb269505c3346f63916bf93457fd76c9d8d436
4ebula <4ebula@gmail.com>
2021-09-23 01:58:03 +0300

    refactor: move product ratings to shared

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -12,7 +12,6 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { ProductPriceComponent } from './components/product-price/product-price.component';
-import { ProductRatingComponent } from './components/product-rating/product-rating.component';
 import { ProductStockComponent } from './components/product-stock/product-stock.component';
 import { CartButtonComponent } from './components/cart-button/cart-button.component';
 import { ProductAvailabilityDirective } from './directives/product-availability/product-availability.directive';
@@ -29,7 +28,6 @@ import { OrderFormComponent } from './components/order-form/order-form.component
     BreadcrumbsComponent,
     ProductPageComponent,
     ProductPriceComponent,
-    ProductRatingComponent,
     ProductStockComponent,
     CartButtonComponent,
     ProductAvailabilityDirective,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductRatingComponent } from '@shared/components/product-rating/product-rating.component';
 import { PopupComponent } from './components/popup/popup.component';
 import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
-  declarations: [PopupComponent, SliderComponent],
+  declarations: [PopupComponent, SliderComponent, ProductRatingComponent],
   imports: [CommonModule],
-  exports: [PopupComponent, SliderComponent],
+  exports: [PopupComponent, SliderComponent, ProductRatingComponent],
 })
 export class SharedModule {}
