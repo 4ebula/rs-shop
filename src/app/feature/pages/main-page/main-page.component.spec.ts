c41e5369d3e9c46cfd368f03f78d3fe2ec7d605a
4ebula <4ebula@gmail.com>
2021-09-23 19:05:06 +0300

    fix: fix main page component test

--- src/app/feature/pages/main-page/main-page.component.spec.ts
+++ src/app/feature/pages/main-page/main-page.component.spec.ts
@@ -1,16 +1,39 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+
+import { ProductsService } from '@feature/services/products/products.service';
 
 import { MainPageComponent } from './main-page.component';
 
 describe('MainComponent', () => {
   let component: MainPageComponent;
   let fixture: ComponentFixture<MainPageComponent>;
+  const prodServTest: Partial<ProductsService> = {
+    getProduct(id: string) {
+      return new Observable((subscriber) => {
+        subscriber.next({
+          id,
+          name: 'Test',
+          imageUrls: [''],
+          availableAmount: 1,
+          price: 111,
+          rating: 5,
+          description: 'Test',
+          isInCart: false,
+          isFavorite: false,
+          category: 'Test',
+          subCategory: 'Test',
+        });
+      });
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MainPageComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{ provide: ProductsService, useValue: prodServTest }],
     }).compileComponents();
   });
 
